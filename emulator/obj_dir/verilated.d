8fe278dd7874aeb7f9f9e1ed623f261fa54fcb2d
arkhan9109 <abdullahrazakhan91@gmail.com>
2021-01-18 21:58:04 +0100

    .

--- emulator/obj_dir/verilated.d
+++
@@ -1,8 +0,0 @@
-verilated.o: /home/ark/Desktop/epi/verilator/include/verilated.cpp \
- /home/ark/Desktop/epi/verilator/include/verilatedos.h \
- /home/ark/Desktop/epi/verilator/include/verilated_imp.h \
- /home/ark/Desktop/epi/verilator/include/verilated.h \
- /home/ark/Desktop/epi/verilator/include/verilated_heavy.h \
- /home/ark/Desktop/epi/verilator/include/verilated_syms.h \
- /home/ark/Desktop/epi/verilator/include/verilated_sym_props.h \
- /home/ark/Desktop/epi/verilator/include/verilated_config.h
--- emulator/obj_dir/verilated_threads.d
+++
@@ -1,5 +0,0 @@
-verilated_threads.o: \
- /home/ark/Desktop/epi/verilator/include/verilated_threads.cpp \
- /home/ark/Desktop/epi/verilator/include/verilatedos.h \
- /home/ark/Desktop/epi/verilator/include/verilated_threads.h \
- /home/ark/Desktop/epi/verilator/include/verilated.h
